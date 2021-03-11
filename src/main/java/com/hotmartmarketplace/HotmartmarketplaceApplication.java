504c3fce7988e89de0f2cf6e3b418865d9c5e60c
andrericardoalves <andrecajuru@gmail.com>
2021-03-11 16:48:55 -0300

    create project e configure connection with database

---
+++ src/main/java/com/hotmartmarketplace/HotmartmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.hotmartmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartmarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotmartmarketplace/HotmartmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotmartmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotmartmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
